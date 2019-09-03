839290e8d085c482f10230849090309c730ce039
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-03 18:43:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+   hash = { => 1}
+  return hash
 end
 
 def read_from_hash(hash, key)
